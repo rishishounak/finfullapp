70c6c1c0dace5b109e8b849bef7a19c6dafb5956
rishishounak <65448224+rishishounak@users.noreply.github.com>
2024-12-07 17:56:02 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -16,9 +16,9 @@ const serverUrl = `http://localhost:${process.env.PORT || 5000}`;
 
 
 
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(cors());
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // app.use(bodyParser.json()); 
 app.use(cors({
