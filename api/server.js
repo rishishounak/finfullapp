be76f20c9c598a92b89af961000cc9d132ca7fb7
rishishounak <65448224+rishishounak@users.noreply.github.com>
2024-12-07 14:29:13 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -52,7 +52,7 @@ agenda.define("send-email", async (job) => {
 });
 
 // Endpoint to handle flowchart save and email scheduling
-app.post("/api/save-flowchart", async (req, res) => {
+app.post("https://finfullapp-rishishounaks-projects.vercel.app/api/save-flowchart", async (req, res) => {
   console.log('this request body is ',req.body);
   const { nodes } = req.body;
 
