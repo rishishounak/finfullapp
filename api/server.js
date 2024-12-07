ccc333261725515566e0bad37253fd0261ff3fe2
rishishounak <65448224+rishishounak@users.noreply.github.com>
2024-12-07 15:33:37 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 // }));
 // app.use(bodyParser.json()); 
 const app = express();
+const PORT = process.env.PORT || 5000;
 const serverUrl = `http://localhost:${process.env.PORT || 5000}`;
 
 
@@ -115,7 +116,7 @@ agenda.on("ready", () => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
