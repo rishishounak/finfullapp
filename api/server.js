6e415f92cc13b1d338440d8a28f7ee053e6e4a15
rishishounak <65448224+rishishounak@users.noreply.github.com>
2024-12-07 15:29:06 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,6 +11,10 @@ const cors = require('cors');
 // }));
 // app.use(bodyParser.json()); 
 const app = express();
+const serverUrl = `http://localhost:${process.env.PORT || 5000}`;
+
+
+
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -52,7 +56,7 @@ agenda.define("send-email", async (job) => {
 });
 
 // Endpoint to handle flowchart save and email scheduling
-app.post("http://localhost:5000/api/save-flowchart", async (req, res) => {
+app.post("${serverUrl}/api/save-flowchart", async (req, res) => {
   console.log('this request body is ',req.body);
   const { nodes } = req.body;
 
